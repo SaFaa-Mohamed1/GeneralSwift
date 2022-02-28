1faab0fbdc4a9f6dda9998128c2ef102dfc443cb
safaa <safaam673@gmail.com>
2022-02-28 12:39:55 +0200

    Add Functions to Date Extention

--- GeneralSwift/GeneralSwift/Sources/UIViewController-Extension.swift
+++ GeneralSwift/GeneralSwift/Sources/UIViewController-Extension.swift
@@ -7,15 +7,39 @@
 
  
 import UIKit
+ 
 extension UIViewController
-{
+
+{   /// Present viewController with completion
+    public func present(_ viewControllerToPresent: UIViewController,
+                        completion: @escaping (() -> ())) {
+        present(viewControllerToPresent, animated: true,
+                completion: completion)
+    }
+    /// Present viewController
+    public func present(_ viewControllerToPresent: UIViewController) {
+        present(viewControllerToPresent, animated: true, completion: nil)
+    }
+    /// add  child ViewController
+    public func addChildViewController(_ viewController: UIViewController) {
+        viewController.willMove(toParent: self)
+        addChild(viewController)
+        viewController.view.frame = view.frame
+        view.addSubview(viewController.view)
+        viewController.didMove(toParent: self)
+    }
+    /// dismiss viewController
+    public func dismiss(completion: (() -> Void)? = nil) {
+        presentingViewController?.dismiss(animated: true, completion: completion)
+    }
+    
     /// Present alert dialog
-   public func presentAlert(message: String,
-                            title: String = "",
-                            addCancelAction : Bool,
-                            oTherTitle : String,
-                            cancelTitle : String , cancelHandler: ((UIAlertAction) -> ())? = nil ,   otherHandler: ((UIAlertAction) -> ())? = nil)
-   {
+    public func presentAlert(message: String,
+                             title: String = "",
+                             addCancelAction : Bool,
+                             oTherTitle : String,
+                             cancelTitle : String , cancelHandler: ((UIAlertAction) -> ())? = nil ,   otherHandler: ((UIAlertAction) -> ())? = nil)
+    {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let oTherAction = UIAlertAction(title: oTherTitle, style: .default, handler: otherHandler)
         let cancelAction = UIAlertAction(title: cancelTitle, style: .cancel, handler: cancelHandler)
@@ -29,13 +53,15 @@ extension UIViewController
     public func presentActionsheet(message: String,
                                    title: String = "",
                                    addCancelAction : Bool,
-                                   okTitle : String,
+                                   oTherTitle : String,
                                    cancelTitle : String,
-                                   from sourceView: UIView)
+                                   from sourceView: UIView,
+                                   cancelHandler: ((UIAlertAction) -> ())? = nil,
+                                   otherHandler: ((UIAlertAction) -> ())? = nil)
     {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
-        let oKAction = UIAlertAction(title: okTitle, style: .default, handler: nil)
-        let cancelAction = UIAlertAction(title: cancelTitle, style: .cancel, handler: nil)
+        let oTherAction = UIAlertAction(title: oTherTitle, style: .default, handler: otherHandler)
+        let cancelAction = UIAlertAction(title: cancelTitle, style: .cancel, handler: cancelHandler)
         if addCancelAction  {
             alertController.addAction(cancelAction)
         }
@@ -44,9 +70,9 @@ extension UIViewController
             alertController.popoverPresentationController?.sourceRect = sourceView.bounds
             alertController.popoverPresentationController?.permittedArrowDirections = [.down, .up]
         }
-        alertController.addAction(oKAction)
+        alertController.addAction(oTherAction)
         self.present(alertController, animated: true, completion: nil)
     }
-
+    
     
 }
--- GeneralSwift/GeneralSwift/Sources/Validator.swift
+++ GeneralSwift/GeneralSwift/Sources/Validator.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 public struct validator {
-    public static func validEmail(_ email : String) -> Bool {
+    public static func isValidEmail(_ email : String) -> Bool {
    
 let regex = try! NSRegularExpression(pattern: "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", options: .caseInsensitive)
         let boolValue =  regex.firstMatch(in: email, options: [], range: NSRange(location: 0, length: email.count)) != nil
@@ -15,13 +15,11 @@ let regex = try! NSRegularExpression(pattern: "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+
     }
    public static func isDifferentPass(_ password:String, _ confirmPassword:String) -> Bool {
             if (password == confirmPassword){
-                return false
-            } else {
                 return true
+            } else {
+                return false
             }
         }
     
-    public static func valid() -> String{
-       return "Email is valid"
-    }
+  
 }
